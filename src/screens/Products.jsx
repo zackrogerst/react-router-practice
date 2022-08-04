985aae8682a8b6da84251b96e716cef72847cbad
Zack Rogers <zack.rogers.t@gmail.com>
2022-08-04 12:44:34 -0600

    added product links, updated slug

--- src/screens/Products.jsx
+++ src/screens/Products.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Products = () => {
 	return (
 		<section>
 			<h1>Products page</h1>
 			<ul>
-				<li>Book</li>
-				<li>Carpet</li>
-				<li>Online Course</li>
+				<li>
+					<Link to="/products/p1">Book</Link>
+				</li>
+				<li>
+					<Link to="/products/p2">Carpet</Link>
+				</li>
+				<li>
+					<Link to="/products/p3">Online Course</Link>
+				</li>
 			</ul>
 		</section>
 	);
