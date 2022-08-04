f0914c9378498b1e15f55dca512adf7231ffef8a
Zack Rogers <zack.rogers.t@gmail.com>
2022-08-04 12:20:21 -0600

    added Link and MainHeader. removed comments from  html

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { Route } from "react-router-dom";
 import Welcome from "./screens/Welcome";
 import Products from "./screens/Products";
+import MainHeader from "./components/MainHeader";
 
 function App() {
 	return (
 		<div>
-			<Route path="/welcome">
-				<Welcome />
-			</Route>
-
-			<Route path="/products">
-				<Products />
-			</Route>
+			<MainHeader />
+			<main>
+				<Route path="/welcome">
+					<Welcome />
+				</Route>
+				<Route path="/products">
+					<Products />
+				</Route>
+			</main>
 		</div>
 	);
 }
